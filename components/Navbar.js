a65f5a6f49a631cca72ae155220d14bc6199b8ea
Omar Sarfraz <omar786089@gmail.com>
2022-09-26 21:49:53 +0500

    Navbar Elements Done

--- components/Navbar.js
+++ components/Navbar.js
@@ -2,11 +2,33 @@ import React from 'react';
 import Image from 'next/image';
 
 import styles from '../styles/navbar.module.css';
+import Link from 'next/link';
 
 export default function Navbar() {
     return (
-        <div className={styles.navbar}>
-            <Image src='/assets/logo.png' width='220px' height='50px'></Image>
+        <div className={styles.main}>
+            <div className={styles.navbar}>
+                <h2>ECommerce</h2>
+                <nav>
+                    <ul className={styles.ul}>
+                        <li><Link href='#'>Apple</Link></li>
+                        <li><Link href='#'>Samsung</Link></li>
+                        <li><Link href='#'>Xiaomi</Link></li>
+                    </ul>
+                </nav>
+                <div className={styles.rightNav}>
+                    <Image src='/assets/cart-icon.svg' width='33px' height='33px' />
+                    <Image src='/assets/profile-icon.svg' width='33px' height='33px' />
+                    <p className={styles.menu}></p>
+                </div>
+            </div>
+            <nav>
+                <ul className={styles.mobileNav}>
+                    <li><Link href='#'>Apple</Link></li>
+                    <li><Link href='#'>Samsung</Link></li>
+                    <li><Link href='#'>Xiaomi</Link></li>
+                </ul>
+            </nav>
         </div>
     );
 }
