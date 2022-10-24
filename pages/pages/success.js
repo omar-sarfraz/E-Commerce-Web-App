cc994a3fdb6155c7b7e532a00a1eced2cd98446b
omar-sarfraz <omar786089@gmail.com>
2022-10-24 10:14:05 -0700

    Success Page

--- pages/pages/success.js
+++ pages/pages/success.js
@@ -1,5 +1,30 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch } from "react-redux";
+
+import styles from "../../styles/success.module.css";
+
+import Link from "next/link";
+import { clearCart } from "../../redux/slices/cartSlice";
+import runOrderAnim from "../../lib/utils";
 
 export default function Success() {
-  return <div>Success</div>;
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(clearCart());
+    runOrderAnim();
+  }, []);
+
+  return (
+    <div className={styles.successOuter}>
+      <div className="container">
+        <div className={styles.success}>
+          <p>Thank you for your purchase ❤</p>
+          <Link href="/">
+            <a className={styles.button}>Explore More</a>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
 }
--- redux/slices/cartSlice.js
+++ redux/slices/cartSlice.js
@@ -1,69 +1,86 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-    value: {
-        products: [],
-        totalPrice: 0,
-        totalProducts: 0
-    },
-}
+  value: {
+    products: [],
+    totalPrice: 0,
+    totalProducts: 0,
+  },
+};
 
 export const cartSlice = createSlice({
-    name: 'cart',
-    initialState,
-    reducers: {
-        addProduct: (state, action) => {
-            const doProductExist = state.value.products.find(item => item._id === action.payload._id)
-            if (doProductExist) {
-                state.value.products.forEach((item, index) => {
-                    if (item._id === action.payload._id) {
-                        state.value.products[index].quantity += action.payload.quantity
-                        state.value.totalPrice += action.payload.quantity * action.payload.price
-                        return
-                    }
-                })
-            } else {
-                state.value.products.push(action.payload)
-                state.value.totalPrice += action.payload.quantity * action.payload.price
-                state.value.totalProducts += 1
-            }
-        },
-        increaseQty: (state, action) => {
-            state.value.products.forEach((item, index) => {
-                if (item._id === action.payload._id) {
-                    state.value.products[index].quantity += 1
-                    state.value.totalPrice += action.payload.price
-                    return
-                }
-            })
-        },
-        decreaseQty: (state, action) => {
-            state.value.products.forEach((item, index) => {
-                if (item._id === action.payload._id) {
-                    if (state.value.products[index].quantity === 1) return
-                    state.value.products[index].quantity -= 1
-                    state.value.totalPrice -= action.payload.price
-                    return
-                }
-            })
-        },
-        removeProduct: (state, action) => {
-            let itemToRemove
-            state.value.products.forEach((item, index) => {
-                if (item._id === action.payload._id) {
-                    itemToRemove = index;
-                }
-            })
-            let priceToDecrease = state.value.products[itemToRemove].quantity * state.value.products[itemToRemove].price
-            state.value.totalProducts -= 1
-            state.value.totalPrice -= priceToDecrease
-            state.value.products.splice(itemToRemove, 1)
-        }
+  name: "cart",
+  initialState,
+  reducers: {
+    addProduct: (state, action) => {
+      const doProductExist = state.value.products.find(
+        (item) => item._id === action.payload._id
+      );
+      if (doProductExist) {
+        state.value.products.forEach((item, index) => {
+          if (item._id === action.payload._id) {
+            state.value.products[index].quantity += action.payload.quantity;
+            state.value.totalPrice +=
+              action.payload.quantity * action.payload.price;
+            return;
+          }
+        });
+      } else {
+        state.value.products.push(action.payload);
+        state.value.totalPrice +=
+          action.payload.quantity * action.payload.price;
+        state.value.totalProducts += 1;
+      }
     },
-})
+    increaseQty: (state, action) => {
+      state.value.products.forEach((item, index) => {
+        if (item._id === action.payload._id) {
+          state.value.products[index].quantity += 1;
+          state.value.totalPrice += action.payload.price;
+          return;
+        }
+      });
+    },
+    decreaseQty: (state, action) => {
+      state.value.products.forEach((item, index) => {
+        if (item._id === action.payload._id) {
+          if (state.value.products[index].quantity === 1) return;
+          state.value.products[index].quantity -= 1;
+          state.value.totalPrice -= action.payload.price;
+          return;
+        }
+      });
+    },
+    removeProduct: (state, action) => {
+      let itemToRemove;
+      state.value.products.forEach((item, index) => {
+        if (item._id === action.payload._id) {
+          itemToRemove = index;
+        }
+      });
+      let priceToDecrease =
+        state.value.products[itemToRemove].quantity *
+        state.value.products[itemToRemove].price;
+      state.value.totalProducts -= 1;
+      state.value.totalPrice -= priceToDecrease;
+      state.value.products.splice(itemToRemove, 1);
+    },
+    clearCart: (state, action) => {
+      state.value.products = [];
+      state.value.totalPrice = 0;
+      state.value.totalProducts = 0;
+    },
+  },
+});
 
-const { actions, reducer } = cartSlice
+const { actions, reducer } = cartSlice;
 
-export const { addProduct, increaseQty, decreaseQty, removeProduct } = actions
+export const {
+  addProduct,
+  increaseQty,
+  decreaseQty,
+  removeProduct,
+  clearCart,
+} = actions;
 
-export default reducer
+export default reducer;
